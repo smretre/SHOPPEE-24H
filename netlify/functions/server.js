1286e8b7044d2e2fefbec4cf8de47a8b885f3893
smretre <smretre909@gmail.com>
2026-02-28 17:54:48 -0300

    Update server.js

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -46,7 +46,7 @@ const mineradorHandler = async (event) => {
 async function buscarOfertasEmAlta() {
     const timestamp = Math.floor(Date.now() / 1000);
     const query = `{
-        getItemList(page: 1, pageSize: 5, region: "BR") {
+        getItemList(keyword: "oferta", page: 1, pageSize: 5) {
             nodes {
                 item_name
                 item_url
