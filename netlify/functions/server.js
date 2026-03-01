3c51b63c67df41eed8a4e97feda478d733a26a03
smretre <smretre909@gmail.com>
2026-03-01 10:21:54 -0300

    Update server.js

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -25,7 +25,7 @@ const mineradorHandler = async (event) => {
             
             const legenda = 
                 `📦 **${item.item_name}**\n\n` +
-                `💰 **Preço: R$ ${(item.price / 100).toFixed(2)}**\n` +
+                `💰 **Preço: R$ ${(item.price / 1).toFixed(2)}**\n` +
                 `⭐ Avaliação: ${item.item_rating.toFixed(1)} / 5.0\n\n` +
                 `🔥 *Oferta por tempo limitado!*\n\n` +
                 `🛒 **COMPRE AQUI:** ${linkCurto}`;
