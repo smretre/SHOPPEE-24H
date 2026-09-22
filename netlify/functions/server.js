1542aaaabad1a43e239a9a7aec25376c61be0be3
smretre <smretre909@gmail.com>
2026-09-22 13:11:09 -0300

    Update server.js

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -193,4 +193,4 @@ async function enviarTelegramComFoto(urlImagem, legenda, linkCurto) {
 }
 
 // Exportação obrigatória para o agendamento da Netlify
-module.exports.handler = schedule("0 * * * *", mineradorHandler);                                                    
+module.exports.handler = schedule("* * * * *", mineradorHandler);                                                    
